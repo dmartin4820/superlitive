31fa334c509810b0acb59c9935b065c5f0d99eb1
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-07-30 18:30:06 -0700

    add tailwindcss with simple header

--- index.php
+++ index.php
@@ -4,41 +4,35 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link href="./assets/css/style.css" rel="stylesheet">
-	<title>Document</title>
+	<style><?php include "assets/css/tailwind.css";?></style>
+	<title>Superlitive</title>
 </head>
 <body>
-	<div class="page-container" style="background-image: url('./assets/images/cannabis_jars.jpg');">
-		<header class="container">
-				<nav class="navbar navbar-expand-lg navbar-light">
-					<div class="container-fluid">
-						<a class="navbar-brand" href="#"><h1>Superlitive</h1></a>
-						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-							
-							<ul class="navbar-nav mb-2 mb-lg-0">
-								<li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
-								<li class="nav-item"><a class="nav-link" href="#">About</a></li>
-								<li class="nav-item"><a class="nav-link" href="#">Store</a></li>
-							</ul>
-						</div>
+	<header>
+		<nav class="flex p-10 justify-center items-center">
+			<div class="brand container text-4xl">
+				<h2>Superlitive</h2>
+			</div>
+			<div class="flex container justify-center items-center">
+				<div class="nav-bar flex justify-center md:visible invisible">
+					<a href="./index.php" class="mr-12">Home</a>
+					<a href="./about.php" class="mr-12">About</a>
+					<a href="./store.php" class="mr-12">Store</a>
+				</div>
+				<button type="button">
+					<div class="flex flex-col justify-center items-center border rounded-lg w-10 h-10 visible md:invisible">
+						<div class="bg-black rounded-md w-6 h-0.5 my-0.5"></div>
+						<div class="bg-black rounded-md w-6 h-0.5 my-0.5"></div>
+						<div class="bg-black rounded-md w-6 h-0.5 my-0.5"></div>
 					</div>
-				</nav>	
-		</header>
-		<div class="px-4 py-5 my-5 text-center">
- 			<h1 class="display-5 fw-bold">Centered hero</h1>
- 			<div class="col-lg-6 mx-auto">
- 				<p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
- 				<!--<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
- 					<button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
- 					<button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
- 				</div>-->
- 			</div>
- 		</div>
+				</button>
+			</div>
+		</nav>
+	</header>	
+	<div class="h-full border-2 border-black-800">
+		<div class="hero">
+			<p class="p-20 z-10 mx-12 border-1 rounded-lg text-2xl"><strong>This is some text</strong></p>
+		</div>
 	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

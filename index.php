25034fd4d580dc5f539d035b7832e25c834ff33d
Dmartin <dom4822@yahoo.com>
2021-09-09 20:52:33 -0700

    change viewport width and add max scale

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86">
+  <meta name="viewport" content="width=630, initial-scale=1, maximum-scale=1">
   <style><?php include "assets/css/tailwind.css";?></style>
   <style><?php include "assets/css/style.css";?></style>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>

90fc8f4b5b3ffa09366ff21a2efc89f9c4e69ff4
Dmartin <dom4822@yahoo.com>
2021-09-09 20:58:25 -0700

    set viewport content to user-scalable=no

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=630, initial-scale=1, maximum-scale=1">
+  <meta name="viewport" content="width=device-width, user-scalable, initial-scale=1, maximum-scale=1">
   <style><?php include "assets/css/tailwind.css";?></style>
   <style><?php include "assets/css/style.css";?></style>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>

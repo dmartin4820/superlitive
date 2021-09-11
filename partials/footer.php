9006093d17ed4333e18d3f179762c28cb7799d90
Dmartin <dom4822@yahoo.com>
2021-09-10 17:38:16 -0700

    add white svg logo and change header and footer background to black

--- partials/footer.php
+++ partials/footer.php
@@ -1,16 +1,16 @@
   <footer class="footer">
-    <div class="flex flex-col md:flex-row justify-evenly p-10 footer-content" data-theme="flex justify-evenly p-10 bg-black">
+    <div class="flex flex-col md:flex-row justify-evenly p-10 footer-content bg-black" data-theme="flex justify-evenly p-10 bg-white">
       <ul class="flex flex-row md:flex-col h-8 md:h-32 justify-between md:pl-20">
-        <!-- <li><a class="black" data-theme="white" href="./about.php">About</a></li> -->
-        <li><a class="black" data-theme="white" href="./index.php">Home</a></li>
-        <li><a class="black" data-theme="white" href="./about.php">Community</a></li>
-        <li><a class="black" data-theme="white" href="./contact.php">Contact</a></li>
-        <li><a class="black" data-theme="white" href="./store.php">Merch</a></li>
-        <li><a class="black" data-theme="white" href="#">Partnerships</a></li>
+        <!-- <li><a class="white" data-theme="white" href="./about.php">About</a></li> -->
+        <li><a class="white" data-theme="white" href="./index.php">Home</a></li>
+        <li><a class="white" data-theme="white" href="./about.php">Community</a></li>
+        <li><a class="white" data-theme="white" href="./contact.php">Contact</a></li>
+        <li><a class="white" data-theme="white" href="./store.php">Merch</a></li>
+        <li><a class="white" data-theme="white" href="#">Partnerships</a></li>
       </ul>
       <div class="flex justify-center items-center brand text-6xl mt-4 md:mb-10 md: mx-2">
-        <img class="superlitive-logo" src="assets/images/superlitive_logos/superlitive_black.svg" data-img="assets/images/superlitive_logos/Black Superlitive Logo.png"width="80" />
-        <h1 class="text-center gold">Super<h1 class="black" data-theme="white">lit</h1><h1 class="text-center gold">ive</h1>
+        <img class="superlitive-logo" src="assets/images/superlitive_logos/superlitive_white.svg" data-img="assets/images/superlitive_logos/white Superlitive Logo.png"width="80" />
+        <h1 class="text-center gold">Super<h1 class="white" data-theme="white">lit</h1><h1 class="text-center gold">ive</h1>
       </div>  
     </div>
   </footer>
--- partials/header.php
+++ partials/header.php
@@ -13,10 +13,10 @@
 
 <body>
   <header class="bg-white">
-    <nav class="flex flex-col p-5 items-center sm:justify-between nav-bar-container-default" data-theme="flex flex-col p-5 items-center justify-between bg-black">
+    <nav class="flex flex-col p-5 items-center sm:justify-between nav-bar-container-default bg-black" data-theme="flex flex-col p-5 items-center justify-between bg-black">
       <div class="flex justify-center items-center brand container text-6xl">
-        <img class="superlitive-logo" src="assets/images/superlitive_logos/superlitive_black.svg" data-img="assets/images/superlitive_logos/White Superlitive Logo.png" width="80" alt="superlitive logo"/>
-        <a href="./index.php"><h1 class="text-center gold" data-theme="text-center white">Super<h1 class="black" data-theme="white">lit</h1><h1 class="text-center gold" data-theme="text-center white">ive</h1></a>
+        <img class="superlitive-logo" src="assets/images/superlitive_logos/superlitive_white.svg" data-img="assets/images/superlitive_logos/White Superlitive Logo.png" width="80" alt="superlitive logo"/>
+        <a href="./index.php"><h1 class="text-center gold" data-theme="text-center white">Super<h1 class="white" data-theme="white">lit</h1><h1 class="text-center gold" data-theme="text-center white">ive</h1></a>
         <button class="nav-btn visible ml-10 md:hidden" type="button">
           <div class="flex flex-col justify-center items-center border-black border-2 rounded-lg w-14 h-14 md:w-10 md:h-10 visible md:hidden">
             <div class="bg-black rounded-md w-6 h-0.5 my-0.5"></div>
@@ -27,10 +27,10 @@
       </div>
       <div class="flex flex-col md:container mt-4 justify-center items-center">
         <div class="nav-bar flex container justify-around">
-          <a href="./index.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base" data-theme="nav-link-white">Home</a>
-          <a href="./about.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base" data-theme="nav-link-white">Community</a> 
-          <a href="./contact.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base" data-theme="nav-link-white">Contact</a>
-          <a href="./store.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base" data-theme="nav-link-white">Merch</a>
+          <a href="./index.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base white" data-theme="nav-link-white">Home</a>
+          <a href="./about.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base white" data-theme="nav-link-white">Community</a> 
+          <a href="./contact.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base white" data-theme="nav-link-white">Contact</a>
+          <a href="./store.php" class="nav-link-default my-2 md:my-0 text-3xl text-center md:text-base white" data-theme="nav-link-white">Merch</a>
           <!-- https://store.superlitiveca.com/checkout.html  -->
         </div>
       </div>
